6fe34f6eca5740c9b5b9e4568247a35883b04dd2
joonho <idaho777@gmail.com>
2014-11-12 00:02:40 -0500

    Header

--- RTP_Project/src/RTPServer.java
+++ RTP_Project/src/RTPServer.java
@@ -1,8 +1,30 @@
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+
 /**
  * 
  * @author Eileen Wang, Joonho Kim
  *
  */
 public class RTPServer {
-
+	
+	String ipAddress;
+	
+	public RTPServer()
+	{
+		
+	}
+	
+	public void openSession()
+	{
+		
+	}
+	
+	
+	
+	public void close()
+	{
+		
+	}
+	
 }
